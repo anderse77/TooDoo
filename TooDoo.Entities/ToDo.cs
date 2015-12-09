95171e1516b52978c95286d4de4b1eaff0eb65ba
Anders Eklöf <anderse77@gmail.com>
2015-12-09 16:30:13 +0100

    Vår första commit.

---
+++ TooDoo.Entities/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TooDoo.Entities
+{
+    public class ToDo
+    {
+        public ToDo() { }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Finnished { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DeadLine { get; set; }
+        public int EstimationTime { get; set; }
+
+    }
+
+}
---
+++ TooDoo.SelfTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TooDoo.SelfTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
