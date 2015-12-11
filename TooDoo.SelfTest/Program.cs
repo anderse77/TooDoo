e1b9903518a0e6faf30195a66259b6a5cca08c7f
Emanjonil <Anthon.Nilsson@gmail.com>
2015-12-11 18:05:31 +0100

    Har lagt till en enkel WCF implementation som den skulle kunna se ut. Och sedan automatiskt genererat ett interface utifrån denna i TooDoo.SelfTest genom att välja Add -> ServiceReferens på projektet. Då hämtas Service interfacet automatiskt från WCF servicen (kan göras utan att projekten ens ligger på samma dator).

--- TooDoo.SelfTest/Program.cs
+++ TooDoo.SelfTest/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TooDoo.SelfTest.ToDoService;
 
 namespace TooDoo.SelfTest
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IToDoService tddoWCF = new ToDoServiceClient();
+			List<ToDo> list = tddoWCF.GetToDoList("Hamid");
+
+			Console.WriteLine(list[0].Description);
+
+			Console.ReadKey();
+		}
+	}
 }
---
+++ TooDoo.Services/IToDoService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using TooDoo.Entities;
+
+namespace TooDoo.Services
+{
+	[ServiceContract]
+	interface IToDoService
+	{
+		[OperationContract]
+		List<ToDo> GetToDoList(string name);
+
+		[OperationContract]
+		bool CreateToDoList(string name);
+
+	}
+}
