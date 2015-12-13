fa950023b56c872d467adec35e2214bd831980ec
Anders Eklöf <anderse77@gmail.com>
2015-12-13 15:37:39 +0100

    Lagt till attribut på interafcet och på servicen för svarsformat och routing

--- TooDoo.Services/IToDoService.cs
+++ TooDoo.Services/IToDoService.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Services;
 using TooDoo.Entities;
 
 namespace TooDoo.Services
@@ -12,6 +14,7 @@ namespace TooDoo.Services
 	public interface IToDoService
 	{
 		[OperationContract]
+        [WebGet(UriTemplate = "gettodolist/{name}", ResponseFormat = WebMessageFormat.Json)]
 		List<ToDo> GetToDoList(string name);
 
 		[OperationContract]
